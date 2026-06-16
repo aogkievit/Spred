1c7b23cabb81223989ee7f414c419cfc67198dee
aogkievit <140564458+aogkievit@users.noreply.github.com>
2026-06-16 01:35:14 -0400

    Minor Progress, Updated README
    There is a lot of work to do, but at least, you can quit the program

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 [Ed](https://linux.die.net/man/1/ed) is a line-based editor.  That means it does not display the text of a file outright unless you issue it a command to output some subset of the lines in a file; even then, the output does not update to cohear with further input.  This might sound like an absurdly antiquated way to edit one's files, but I have found that it provides a more beautiful experience.  Further, I find that it encourages brevity, which I consider a virtue especially while coding.  I would not necessarily recommend using Ed for large projects, but I would encourage you to look into it and see for yourself how you might fit it into your workflow.
 
 Spred attempts to extend the structure of Ed to the editing of spreadsheets.
+
+## Commands
+
+```Q```	Quits the program.
+```q```	Quits the program if (a) there are no unsaved changes or (b) the command has been entered twice in a row.
--- spred.c
+++ spred.c
@@ -1,32 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 
-const char* prompt = "*";
+struct String {
+	char *str;
+	size_t sz; // Size
+	size_t szu; // Size Used
+};
+
+const char *prompt = "*";
+const char *error = "?\n";
 
 static struct termios terminalConfig;
 
+// Options
 static char optionPrompt = 0;
+static char optionHelp = 0;
 
-static void
+static char saved = 1;
+static char fq = 0;
+
+static int
+processCommand(struct String inLine)
+{
+	switch (*inLine.str) {
+		case 'Q':
+			if (inLine.str[1] == '\n') return 1;
+			else {
+				fputs(error, stderr);
+				return 0;
+			}
+			
+		case 'q':
+			if ((saved || fq) && inLine.str[1] == '\n') return 1;
+			else {
+				if (inLine.str[1] == '\n') fq = 1;
+				fputs(error, stderr);
+				return 0;
+			}
+	
+		default:
+			fq = 0;
+			fputs(error, stderr);
+			return 0;
+	}
+}
+
+static int
+input(struct String *inLine)
+{
+	if (inLine->str != NULL) free(inLine->str);
+	inLine->str = (char*) malloc(20 * sizeof(char));
+	if (inLine->str == NULL) return 2;
+	inLine->sz = 20;
+	inLine->szu = 0;
+
+	int c = 0;
+	
+	for (; c != EOF && c != '\n'; c = getc(stdin)) {
+		if (c == '\r' || c == '\0') continue;
+		
+		inLine->str[inLine->szu] = (char) c; ++inLine->szu;
+		
+		//if (inLine->szu == inLine->sz) {
+		//	char *p = malloc((inLine->sz + 20) * sizeof(char));
+		//	if (p == NULL) return 2;
+		//	
+		//	memcpy((void*) p, (void*) inLine->str, (inLine->sz + 20) * sizeof(char));
+		//	free(inLine->str); inLine->str = p;
+		//	inLine->sz = inLine->sz + 20;
+		//}
+	}
+	
+	if (c == EOF) return 2;
+	else {
+		inLine->str[inLine->szu] = '\n';
+		++inLine->szu;
+		
+		return 0;
+	}
+}
+
+static int
 edit(void)
 {
+	struct String inLine = {NULL, 0, 0};
+	
 	for (;;) {
 		if (optionPrompt) {
 			fputs(prompt, stdout);
 			fflush(stdout);
 		}
 
-		getc(stdin);
+		{
+			int r = input(&inLine);
+			if(r) return r;
+			
+			r = processCommand(inLine);
+			if(r) return r;
+		}
 	}
 }
 
 static int
 init(void)
 {
-	if (tcgetattr(fileno(stdin), &terminalConfig)) return 1;
-	terminalConfig.c_cc[VEOL] = '\t';
-	if (tcsetattr(fileno(stdin), TCSANOW, &terminalConfig)) return 1;
+	// TODO: Should only apply when inputing data
+	// if (tcgetattr(fileno(stdin), &terminalConfig)) return 1;
+	// terminalConfig.c_cc[VEOL] = '\t';
+	// if (tcsetattr(fileno(stdin), TCSANOW, &terminalConfig)) return 1;
 
 	// TODO: Register Signal Handlers
 
@@ -35,14 +118,14 @@ init(void)
 	
 
 int
-main (int argc, char** argv)
+main (int argc, char *argv[])
 {
 	// TODO: Handle Arguments
 
-	if (int t = init()) return t;
+	{
+		int r = init();
+		if (r) return r;
+	}
 
-	edit();
-
-	// Never Reached
-	return 0;
+	return (edit() - 1);
 }
